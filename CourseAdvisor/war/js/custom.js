cce161886ec86bc28750add1270f73975289e0ec
Owner <Owner@Virat_Chopra>
2015-02-07 03:56:03 +0800

    Updated Files and Newsfeed UI Complete

--- CourseAdvisor/war/js/custom.js
+++ CourseAdvisor/war/js/custom.js
@@ -27,3 +27,49 @@ $(document).ready(function() {
 	    $('.row-offcanvas').toggleClass('active');
 	  });
 	});
+
+/* var citynames = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: {
+    url: 'assets/citynames.json',
+    filter: function(list) {
+      return $.map(list, function(cityname) {
+        return { name: cityname }; });
+    }
+  }
+});
+citynames.initialize();
+
+$('input').tagsinput({
+  typeaheadjs: {
+    name: 'citynames',
+    displayKey: 'name',
+    valueKey: 'name',
+    source: citynames.ttAdapter()
+  }
+}); */
+
+var citynames = new Bloodhound({
+	  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  prefetch: {
+	    url: 'assets/citynames.json',
+	    filter: function(list) {
+	      return $.map(list, function(cityname) {
+	        return { name: cityname }; });
+	    }
+	  }
+	});
+	citynames.initialize();
+
+	$('.typeaheadtest').tagsinput({
+	  typeaheadjs: {
+	    name: 'citynames',
+	    tagClass: 'wide',
+	    displayKey: 'name',
+	    valueKey: 'name',
+	    source: citynames.ttAdapter()
+	  }
+	});
+
